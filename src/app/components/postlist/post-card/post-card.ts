5bb4e79028a9bb6d311605644b271706078a0a4d
Amit Dubey <amitdubey1826.com>
2026-02-25 14:03:14 +0530

    added relative time pipe and abbreviate number pipe

--- src/app/components/postlist/post-card/post-card.ts
+++ src/app/components/postlist/post-card/post-card.ts
@@ -1,15 +1,17 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { IPost } from '../../../common/post.model';
 import { Highlight } from '../../../directives/highlight';
-import { DatePipe, DecimalPipe, TitleCasePipe } from '@angular/common';
+import { TitleCasePipe } from '@angular/common';
+import { AbbreviateNumberPipe } from '../../../common/abbreviate-number.pipe';
+import { RelativeTimePipe } from '../../../common/pipes/relative-time-pipe';
 
 @Component({
   selector: 'app-post-card',
   imports: [
     Highlight,
     TitleCasePipe,
-    DecimalPipe,
-    DatePipe
+    RelativeTimePipe,
+    AbbreviateNumberPipe
   ],
   templateUrl: './post-card.html',
   styleUrl: './post-card.scss'
